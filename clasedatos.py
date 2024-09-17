453304b53f63794547b807ef465ba8a5177819bc
Joselyn-Alejandra-Valenzuela-0412 <164534000+Joselyn-Alejandra-Valenzuela-0412@users.noreply.github.com>
2024-09-17 12:07:53 -0600

    Add files via upload
    subi archivo clases version 2

---
+++ clasedatos.py
@@ -0,0 +1,31 @@
+class Informacion:
+    
+    def milista(self):
+        lista_Nomperros=["boby","dollar","fany"]
+        for x in lista_Nomperros:
+            print(x)
+    def mitupla(self):
+        tuplaraza=("golden","corgi","doberman")
+        for tup in tuplaraza:
+            print(tup)   
+    def miconjunto(self):
+        conj_edad={"2","3","6"}
+        for con in conj_edad:
+            print(con)    
+    def midiccionario(self):
+        dicci_color={"boby": "beige",
+            "dollar": "café",
+            "fany": "negro"}
+        for di, key in dicci_color.items():
+            print(di, " = " ,key) 
+# creando el objeto
+
+datos=Informacion()
+print("listado de nombres de perros")
+datos.milista()
+print("tupla raza de perros")
+datos.mitupla()
+print("conjunto edad de perros")
+datos.miconjunto()
+print("diccionario color de perro")
+datos.midiccionario()
